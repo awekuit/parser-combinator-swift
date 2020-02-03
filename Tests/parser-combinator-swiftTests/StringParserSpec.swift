6383056666ad0c287b7a09277dadd6d1a2b30d67
awekuit <awekuit@gmail.com>
2020-02-03 19:14:50 +0900

    Revert "Modify argument to `call by reference`"
    This reverts commit fcbc1273

--- Tests/parser-combinator-swiftTests/StringParserSpec.swift
+++ Tests/parser-combinator-swiftTests/StringParserSpec.swift
@@ -9,32 +9,32 @@ class StringParserSpec: QuickSpec {
     override func spec() {
         describe("one") {
             it("hoge") {
-                var s = "abc"
-                let result = try! P.one.parse(&s, s.startIndex).unwrap()
+                let s = "abc"
+                let result = try! P.one.parse(s, s.startIndex).unwrap()
                 expect("a").to(equal(result))
             }
             it("one.rep(1)") {
-                var s = "abc"
-                let result = try! P.one.rep(1).map { String($0) }.parse(&s, s.startIndex).unwrap()
+                let s = "abc"
+                let result = try! P.one.rep(1).map { String($0) }.parse(s, s.startIndex).unwrap()
                 expect("abc").to(equal(result))
             }
         }
         describe("string") {
             it("hoge") {
-                var s = "abc"
-                let result = try! P.string(s).parse(&s, s.startIndex).unwrap()
+                let s = "abc"
+                let result = try! P.string(s).parse(s, s.startIndex).unwrap()
                 expect(s).to(equal(result))
             }
         }
         describe("log") {
             it("short") {
-                var s = "abc"
-                try! P.one.log("one").parse(&s, s.startIndex)
+                let s = "abc"
+                try! P.one.log("one").parse(s, s.startIndex)
             }
             it("long") {
-                var s = "123456789012345***678901234567890"
+                let s = "123456789012345***678901234567890"
                 let parser = P.digits ~> P.string("***").log("aster")
-                try! parser.parse(&s, s.startIndex)
+                try! parser.parse(s, s.startIndex)
             }
         }
     }
