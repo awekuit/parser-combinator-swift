4c4d7500928e8d51bdeb28aec7894af7e80d63a4
awekuit <awekuit@gmail.com>
2020-02-18 17:06:07 +0900

    Add UTF8CStringParserSpec

---
+++ Tests/parser-combinator-swiftTests/UTF8CStringParserSpec.swift
@@ -0,0 +1,26 @@
+import Foundation
+import Nimble
+import ParserCombinatorSwift
+import Quick
+
+class UTF8CStringParserSpec: QuickSpec {
+    private typealias P = ParserCombinatorSwift.UTF8CStringParser
+
+    override func spec() {
+        it("one") {
+            let input = "abc".utf8CString
+            let output = try! P.one.parse(input, input.startIndex).unwrap()
+            expect(input.first!).to(equal(output))
+        }
+        it("one.rep(1)") {
+            let input = "abc".utf8CString
+            let output = try! P.one.rep(1).parse(input, input.startIndex).unwrap()
+            expect(input.dropLast()).to(equal(output)) // `dropLast()` means to remove Null Terminated
+        }
+        it("string") {
+            let input = "abc"
+            let output = try! P.string(input).parse(input.utf8CString, input.utf8CString.startIndex).unwrap()
+            expect(input).to(equal(output))
+        }
+    }
+}
