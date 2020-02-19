5dadea6e1599fe1f75cd0cf23729525e4821b670
awekuit <awekuit@gmail.com>
2020-02-19 14:45:53 +0900

    Cosmetic change

--- Sources/parser-combinator-swift/Core/Operators.swift
+++ Sources/parser-combinator-swift/Core/Operators.swift
@@ -2,7 +2,7 @@ prefix operator &&
 prefix operator !
 
 public func | <Source, Result>(lhs: Parser<Source, Result>, rhs: Parser<Source, Result>) -> Parser<Source, Result> {
-    lhs.or(try rhs)
+    lhs.or(rhs)
 }
 
 public prefix func && <Source, Result>(lhs: Parser<Source, Result>) -> Parser<Source, Void> {
