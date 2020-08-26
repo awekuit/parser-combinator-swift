1f9637c23dd6c4bdb363d20416f8699ee84eb3b9
awekuit <awekuit@gmail.com>
2020-08-26 18:25:16 +0900

    Added `final` keyword to improve performance

--- Sources/parser-combinator-swift/Core/Parser.swift
+++ Sources/parser-combinator-swift/Core/Parser.swift
@@ -5,7 +5,7 @@ public class Parser<Input, Output> where Input: Collection {
     public typealias ParseFunction = (Input, Input.Index) throws -> ParseResult<Input, Output>
 
     /// The wrapped function, call to start the parsing process.
-    private let parseFunction: ParseFunction
+    private final let parseFunction: ParseFunction
 
     /// Initialize a parser with the given wrapping function.
     ///
@@ -18,7 +18,7 @@ public class Parser<Input, Output> where Input: Collection {
     ///
     /// - Parameter input: the token sequence that should be parsed
     /// - Returns: the result of the parsing operation
-    public func parse(_ input: Input, _ index: Input.Index) throws -> ParseResult<Input, Output> {
+    public final func parse(_ input: Input, _ index: Input.Index) throws -> ParseResult<Input, Output> {
         try parseFunction(input, index)
     }
 
@@ -53,7 +53,7 @@ public class Parser<Input, Output> where Input: Collection {
     ///
     /// - Parameter tranform: function that maps a parse result to a new parser
     /// - Returns: a new parser that combines both parse operations.
-    public func flatMap<B>(_ tranform: @escaping (Output) throws -> Parser<Input, B>) -> Parser<Input, B> {
+    public final func flatMap<B>(_ tranform: @escaping (Output) throws -> Parser<Input, B>) -> Parser<Input, B> {
         Parser<Input, B> { input1, index1 in
             try self.parse(input1, index1).flatMap { output, input2, index2 in
                 try tranform(output).parse(input2, index2)
@@ -65,7 +65,7 @@ public class Parser<Input, Output> where Input: Collection {
     ///
     /// - Parameter transform: transforming function that maps from R to B
     /// - Returns: a new parser that calls f on each successful parsing operation
-    public func map<B>(_ transform: @escaping (Output) throws -> B) -> Parser<Input, B> {
+    public final func map<B>(_ transform: @escaping (Output) throws -> B) -> Parser<Input, B> {
         Parser<Input, B> { input, index in
             try self.parse(input, index).map { output, _, _ in
                 try transform(output)
@@ -73,7 +73,7 @@ public class Parser<Input, Output> where Input: Collection {
         }
     }
 
-    public func filter(_ pred: @escaping (Output) -> Bool) -> Parser<Input, Output> {
+    public final func filter(_ pred: @escaping (Output) -> Bool) -> Parser<Input, Output> {
         let failure = ParseResult<Input, Output>.failure(Errors.filtered)
         return Parser<Input, Output> { input, index in
             let r1 = try self.parse(input, index)
